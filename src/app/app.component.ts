56476e5654f97f9dd166b6a4620abfa9a21750be
Dmitry Slepchenko <dmitrys@interlink-ua.com>
2016-08-01 15:16:56 +0300

    init base project structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_INPUT_DIRECTIVES, MdIcon],
+  providers: [MdIconRegistry]
 })
 export class AppComponent {
   title = 'app works!';
